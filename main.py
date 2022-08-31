f000c78b1f6eab9e9697e313b7cc30723f8bb4a9
domi9 <57841773+domi9git@users.noreply.github.com>
2022-08-31 22:05:08 +0300

    the json update

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from nextcord.ext import commands
 import nextcord
-import requests
 import json
+import os
 TESTING_GUILD_ID = guild_id  # Replace with your guild ID
 
 bot = commands.Bot()
@@ -10,18 +10,35 @@ bot = commands.Bot()
 async def on_ready():
     print(f'We have logged in as {bot.user}')
 
-@bot.slash_command(description="the hi command", guild_ids=[TESTING_GUILD_ID])
-async def hi(interaction: nextcord.Interaction,
-    obj: nextcord.User = nextcord.SlashOption(
-        name="object",
-        description="name of the object",
-        required=False
-        )
+@bot.slash_command(description="find stuff in the surrounding area")
+async def find(interaction: nextcord.Interaction, guild_ids=TESTING_GUILD_ID
     ):
     await interaction.response.defer(with_message=True)
     with interaction.channel.typing():
-        if not obj:
-            await interaction.send(f'hello {interaction.user.mention}')
+        userid = interaction.user.id
+        if os.path.exists('json/db.json'):
+            print('db json file exists')
+            with open('json/db.json','r') as file:
+                load = json.load(file)
+                if str(userid) in load[0]:
+                    print('this user is in the db')
+                else:
+                    print('this user isnt in the db, adding...')
+                    with open("json/db.json", "w") as file:
+                        load[0].update({str(userid):{}})
+                        load[0][str(userid)].update({"inv":{}})
+                        load[0][str(userid)].update({"map":{}})
+                        file.write(json.dumps(load, indent=4))
+                        print('this user has now been added')
         else:
-            await interaction.send(f'hello @{obj}')
+            with open('json/db.json','w') as file:
+                dictionary = [{
+                    str(userid): {
+                        "inv": {},
+                        "map": {}
+                    }
+                }]
+                file.write(json.dumps(dictionary, indent=4))
+                print('db created')
+        await interaction.send('done')
 bot.run(token)
